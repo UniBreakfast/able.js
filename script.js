07ea78f65815103c77d313573c7aa53c431569c0
UniBreakfast <unibreakfast1@gmail.com>
2019-08-29 14:12:25 +0300

    init demo

---
+++ script.js
@@ -0,0 +1,12 @@
+app.people = [
+  {name: {first: 'John', last: 'Doe'}, age: 33, height: 178}, 
+  {name: {first: 'Ron', last: 'Crow'}, age: 37, height: 184}, 
+  {name: {first: 'Mary', last: 'May'}, age: 23, height: 168} 
+]
+
+app.dictionary = {
+  tinker: "a person who travels from place to place mending metal utensils as a way of making a living",
+  tailor: "a person whose occupation is making fitted clothes such as suits, pants, and jackets to fit individual customers",
+  soldier: "a person who serves in an army",
+  spy: "a person who secretly collects and reports information on the activities, movements, and plans of an enemy or competitor"
+}
