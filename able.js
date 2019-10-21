c395be1dbdd192844889cff75dddc1305f5ed068
UniBreakfast <unibreakfast1@gmail.com>
2019-10-21 21:43:52 +0300

    app.source new arr if none

--- able.js
+++ able.js
@@ -31,7 +31,8 @@ addEventListener('load', () => {
   
   document.querySelectorAll('.renderable').forEach(area => {
     const template = area.innerHTML.replace(/^\s*<template>|<\/template>\s*$/g, ''),
-          callbacks = [], srcProp = area.dataset.src, source = app[srcProp], 
+          callbacks = [], srcProp = area.dataset.src, 
+          source = app[srcProp] = app[srcProp] || [], 
           placeholders = [...new Set(template.match(/\{.+?\}/g))]
             .reduce((dic, holder) => ({...dic, [holder]: holder
               .split(/[.{}[\]]/g).filter(v => v)}), {});
